cc39d7d4e06263420ef073c6ddeb92e8b828cb5a
yunyuyuan <1607439239@qq.com>
2020-12-11 16:58:06 +0800

    connnect

--- windows/connect/connect.cpp
+++ windows/connect/connect.cpp
@@ -1,97 +1,175 @@
-#include <QtWidgets/QLabel>
-#include <Qt>
 #include "connect.h"
 
-void ConnectWindow::setupUi() {
-    this->tab_create = new Button("创建", this);
-    this->tab_join = new Button("加入", this);
-    this->stop = new Button("取消", this);
-    this->start = new Button("提交", this);
-    connect(this->tab_create, &QPushButton::clicked, this, &ConnectWindow::switchToCreate);
-    connect(this->tab_join, &QPushButton::clicked, this, &ConnectWindow::switchToJoin);
-    connect(this->start, &QPushButton::clicked, this, &ConnectWindow::doConnect);
+const char *qstring_to_char(const QString &s) {
+    const std::string &text = s.toStdString();
+    return text.c_str();
+}
 
-    this->label_ip = new QLabel("地址:", this);
-    this->input_nick = new QLineEdit(this);
-    this->input_ip = new QLineEdit(this);
-    this->status = new QLabel("", this);
-    this->status->setAlignment(Qt::AlignCenter);
+
+void ConnectWindow::setupUi() {
+    tab_create = new Button("创建", this);
+    tab_join = new Button("加入", this);
+    stop = new Button("取消", this);
+    start = new Button("提交", this);
+    connect(tab_create, &QPushButton::clicked, this, &ConnectWindow::switchToCreate);
+    connect(tab_join, &QPushButton::clicked, this, &ConnectWindow::switchToJoin);
+    connect(stop, &QPushButton::clicked, this, &ConnectWindow::cancelConnect);
+    connect(start, &QPushButton::clicked, this, &ConnectWindow::doConnect);
+
+    input_nick = new QLineEdit(this);
+    input_ip = new QLineEdit(this);
+    connect(input_ip, &QLineEdit::textChanged, this, &ConnectWindow::parseIp);
+    
+    status = new QLabel("", this);
+    status->setAlignment(Qt::AlignCenter);
 
     auto* layout = new QGridLayout(this);
     layout->addWidget(new QLabel("昵称:", this), 0, 0, 1, 1);
-    layout->addWidget(this->input_nick, 0, 1, 1, 5);
-    layout->addWidget(this->label_ip, 1, 0, 1, 1);
-    layout->addWidget(this->input_ip, 1, 1, 1, 5);
-    layout->addWidget(this->tab_create, 2, 0, 1, 1);
-    layout->addWidget(this->tab_join, 2, 1, 1, 1);
-    layout->addWidget(this->stop, 2, 4, 1, 1);
-    layout->addWidget(this->start, 2, 5, 1, 1);
-    layout->addWidget(this->status, 3, 0, 1, 6);
-    this->setLayout(layout);
+    layout->addWidget(input_nick, 0, 1, 1, 11);
+    layout->addWidget(new QLabel("地址:", this), 1, 0, 1, 1);
+    layout->addWidget(input_ip, 1, 1, 1, 11);
+    layout->addWidget(tab_create, 2, 0, 1, 2);
+    layout->addWidget(tab_join, 2, 2, 1, 2);
+    layout->addWidget(stop, 2, 8, 1, 2);
+    layout->addWidget(start, 2, 10, 1, 2);
+    layout->addWidget(status, 3, 0, 1, 12);
+    setLayout(layout);
 }
 
 ConnectWindow::ConnectWindow() {
-    this->setupUi();
-    this->show();
+    setupUi();
+    show();
 }
-
 
 void ConnectWindow::switchToCreate() {
-    this->label_ip->setText("端口:");
-    this->is_create = true;
+    is_create = true;
 }
-
 void ConnectWindow::switchToJoin() {
-    this->label_ip->setText("地址:");
-    this->is_create = false;
+    is_create = false;
+}
+void ConnectWindow::parseIp() {
+    const QString &s = input_ip->text().trimmed();
+    if (s.indexOf(":") == -1){
+        return;
+    }
+    QStringList lis = s.split(":");
+    input_ip_text = lis[0];
+    input_port_text = lis[1].toInt();
 }
 
 void ConnectWindow::doConnect() {
-    if (this->connecting)return;
-    this->connecting = true;
-    this->status->setText("正在连接...");
-    if (this->is_create){
-        this->sock = this->SocketServer();
+    if (connecting)return;
+    connecting = true;
+    status->setText("准备连接...");
+    if (is_create) {
+        auto worker = new ServerWorker(this->input_port_text, qstring_to_char(this->input_nick->text()));
+        connect(worker, &ServerWorker::resultReady, this, &ConnectWindow::server_finish);
+        connect(worker, &ServerWorker::statusChange, this, &ConnectWindow::status_change);
+        worker->start();
     }else{
-        this->sock = this->SocketClient();
+        auto worker = new ClientWorker(this->input_port_text, qstring_to_char(this->input_ip_text), qstring_to_char(this->input_nick->text()));
+        connect(worker, &ClientWorker::resultReady, this, &ConnectWindow::client_finish);
+        connect(worker, &ClientWorker::statusChange, this, &ConnectWindow::status_change);
+        worker->start();
     }
-    if (this->sock == 0){
-        this->status->setText((QString)(this->is_create?"创建":"加入") + "失败");
-    }
-    this->connecting = false;
+}
+void ConnectWindow::status_change(const char* str) {
+    status->setText(str);
 }
 
-int ConnectWindow::SocketServer() {
-    return 0;
+void ConnectWindow::server_finish(int sock_, int client_sock_) {
+    this->sock = sock_;
+    this->client_sock = client_sock_;
+    if (client_sock == 0){
+        status->setText("创建失败");
+    }else{
+        status->setText("创建成功");
+    }
+    if (sock == 0){
+    }
+    connecting = false;
+}
+void ConnectWindow::client_finish(int sock_) {
+    this->sock = sock_;
+    if (sock == 0){
+        status->setText("加入失败");
+    }else{
+        status->setText("加入成功");
+    }
+    connecting = false;
+}
+
+void ConnectWindow::cancelConnect() {
+
+}
+
+// ------- worker ----------
+
+ServerWorker::ServerWorker(uint16_t input_port_text, const char* input_nick) {
+    this->input_port_text = input_port_text;
+    this->input_nick = input_nick;
+}
+void ServerWorker::run() {
     struct sockaddr_in address;
     int addrlen = sizeof(address);
     int opt = 1;
-    int new_socket;
+    char buffer[1024] = {0};
 
+    emit statusChange("创建服务器...");
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == 0){
-        return 0;
     }
-    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR|SO_REUSEPORT, &opt, sizeof(opt))){
-        return 0;
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
     }
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(8886);
+    address.sin_port = htons(input_port_text);
+    emit statusChange("正在绑定端口...");
     if (bind(sock, (struct sockaddr*)&address, sizeof(address)) < 0){
-        return 0;
     }
-    if (listen(sock, 1)<0){
-        return 0;
+    emit statusChange("准备监听连接...");
+    if (listen(sock, 1) < 0){
     }
-    status->setText("等待玩家连接...");
-    if ((new_socket = accept(sock, (struct sockaddr *)&address ,(socklen_t *) &addrlen))< 0){
-        return 0;
+    emit statusChange("等待玩家连接...");
+    int client_sock;
+    if ((client_sock = accept(sock, (struct sockaddr *)&address , (socklen_t *) &addrlen)) < 0){
     }
-
-    return sock;
+    // 发送主机昵称
+    emit statusChange("发送自身昵称...");
+    send(sock , input_nick , strlen(input_nick) , 0);
+    // 接受客机昵称
+    emit statusChange("接受对方昵称...");
+    recv(sock , buffer, 1024, 0);
+    printf("%s\n", buffer);
+    emit resultReady(sock, client_sock);
 }
 
-int ConnectWindow::SocketClient() {
-    return 0;
+ClientWorker::ClientWorker(uint16_t input_port_text, const char * input_ip_text, const char * input_nick) {
+    this->input_port_text = input_port_text;
+    this->input_ip_text = input_ip_text;
+    this->input_nick = input_nick;
+}
+
+void ClientWorker::run() {
+    struct sockaddr_in serv_addr;
+    int sock;
+    char buffer[1024] = {0};
+    emit statusChange("正在创建连接...");
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+    }
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(uint16_t(input_port_text));
+    if(inet_pton(AF_INET, input_ip_text, &serv_addr.sin_addr)<=0){
+    }
+    emit statusChange("正在连接主机...");
+    if (::connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+    }
+    // 接受主机昵称
+    emit statusChange("接受对方昵称...");
+    recv(sock , buffer, 1024, 0);
+    printf("%s\n", buffer);
+    // 发送客机昵称
+    emit statusChange("发送自身昵称...");
+    auto nick = qstring_to_char(input_nick);
+    send(sock , nick , strlen(nick) , 0);
 }
--- windows/connect/connect.h
+++ windows/connect/connect.h
@@ -1,26 +1,35 @@
 #include <QtWidgets/QWidget>
+#include <QObject>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QLayout>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QLabel>
+#include <QtCore/Qt>
+#include <QtCore/QThread>
 #include "../../components/button/button.h"
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
-class ConnectWindow: public QWidget{
+class ConnectWindow: public QWidget {
 public:
     Button* tab_create;
     Button* tab_join;
     Button* stop;
     Button* start;
 
-    QLabel* label_ip;
     QLineEdit* input_nick;
     QLineEdit* input_ip;
     QLabel* status;
 
     bool connecting = false;
     bool is_create = true;
-    int sock = 0;
+    int sock, client_sock = 0;
+    QString input_ip_text;
+    uint16_t input_port_text;
 
     void setupUi();
     explicit ConnectWindow();
@@ -28,7 +37,38 @@ public:
 private:
     void switchToCreate();
     void switchToJoin();
+    void parseIp();
+
     void doConnect();
-    int SocketServer();
-    int SocketClient();
+    void status_change(const char*);
+    void server_finish(int, int);
+    void client_finish(int);
+    void cancelConnect();
 };
+
+// ********** worker **********
+class ServerWorker : public QThread{
+Q_OBJECT
+public:
+    uint16_t input_port_text;
+    const char* input_nick;
+    ServerWorker(uint16_t, const char*);
+    void run() override;
+signals:
+    void statusChange(const char* str);
+    void resultReady(const int sock, const int client);
+};
+
+class ClientWorker : public QThread{
+Q_OBJECT
+public:
+    uint16_t input_port_text;
+    const char* input_ip_text;
+    const char* input_nick;
+    ClientWorker(uint16_t, const char*, const char*);
+    void run() override;
+signals:
+    void statusChange(const char* str);
+    void resultReady(const int sock);
+};
+
